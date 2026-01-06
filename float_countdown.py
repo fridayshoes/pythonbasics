47a958c4c3f68217841a108ad75ebaf7155a1e4b
fridayshoes <one_amp@hotmail.com>
2026-01-06 22:37:09 +0000

    countdown using floats

---
+++ float_countdown.py
@@ -0,0 +1,23 @@
+# Countdown with floats
+
+value = 5.0
+
+while value > 0:
+  print("Value", round(value, 1))
+  value -= 0.5
+else:
+  print("Countdown complete")
+
+# Output
+
+# Value 5.0
+# Value 4.5
+# Value 4.0
+# Value 3.5
+# Value 3.0
+# Value 2.5
+# Value 2.0
+# Value 1.5
+# Value 1.0
+# Value 0.5
+# Countdown complete
