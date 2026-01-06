2e68c624ec77766c103c12d96a35f754545b3314
fridayshoes <one_amp@hotmail.com>
2026-01-06 12:45:08 +0000

    Converting miles to kilometres

---
+++ float_convert_miles_kilometres.py
@@ -0,0 +1,13 @@
+# Convert miles to kilometres
+
+miles = 5.0
+km = miles * 1.60934
+
+if km > 8:
+  print("Distance in km", km)
+else:
+  print("Short distance.")
+
+# Output
+
+# Distance in km 8.0467
