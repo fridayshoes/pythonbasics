2998eb15de7c1c55b0f6520b8024a5e8466028d3
fridayshoes <one_amp@hotmail.com>
2026-01-06 12:19:33 +0000

    Rounding a price from a float

---
+++ float_rounding_off_price.py
@@ -0,0 +1,13 @@
+# Round off a float to the nearest whole number
+
+price = 19.65
+rounded_price = round(price)
+
+if rounded_price >= 20:
+  print("Rounded price:", rounded_price)
+else:
+  print("Price rounded down.")
+
+# Output
+
+# Rounded price: 20
