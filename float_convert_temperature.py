4cc34449294f403062281b3f047ecdf04733890c
fridayshoes <one_amp@hotmail.com>
2026-01-06 12:13:11 +0000

    convert temp from celsius to fareheit

---
+++ float_convert_temperature.py
@@ -0,0 +1,13 @@
+# Convert celsius to farenheit
+
+celsius = 37.0
+farenheit = (celsius * 9/5) + 32
+
+if farenheit > 98:
+  print("The temperature in Farenheit is", farenheit)
+else:
+  print("Temperature is below normal.")
+
+# Output
+
+# The temperature in Farenheit is 98.6
