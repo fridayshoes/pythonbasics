c99897f4bda190f6a7a41a099473a0153382608f
fridayshoes <one_amp@hotmail.com>
2026-01-06 22:33:00 +0000

    Calculate mileage using float division.

---
+++ float_fuel_consumption.py
@@ -0,0 +1,14 @@
+# Calculate mileage
+
+distance = 150.00
+fuel_used = 10.0
+mileage = distance / fuel_used
+
+if mileage >= 15.0:
+  print("Mileage:", mileage, "km per litre.")
+else:
+  print("Mileage below expected.")
+
+# Output
+
+# Mileage: 15.0 km per litre.
