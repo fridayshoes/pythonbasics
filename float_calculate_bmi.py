251a8a19e50cbf8ebd47d215319336b90bfe53f8
fridayshoes <one_amp@hotmail.com>
2026-01-06 22:14:48 +0000

    Use floats in BMI calculation and if-elif

---
+++ float_calculate_bmi.py
@@ -0,0 +1,16 @@
+# Calculate Body Mass Index (BMI)
+
+weight = 68.0 # kg
+height = 1.65 # metres
+bmi = weight / (height * height)
+
+if bmi < 18.5:
+  print("Underweight")
+elif bmi < 18.5:
+  print("Normal weight")
+else:
+  print("Overweight")
+
+# Output
+
+# Overweight
