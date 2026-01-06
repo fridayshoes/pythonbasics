060949dbfa3333a13f9bfab2a9cf24df452de9df
fridayshoes <one_amp@hotmail.com>
2026-01-06 12:26:48 +0000

    calculate the average score from a list of scores

---
+++ float_average_test_scores.py
@@ -0,0 +1,15 @@
+# Calculate average test scores
+
+scores = [80.5, 90.0, 75.5]
+total = 0.0
+
+for score in scores:
+  total += score
+else:
+  average = total / len(scores)
+  print("Average score:", average)
+
+
+# Output
+
+# Average score: 82.0
