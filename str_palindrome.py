8e3a2cb5772099baa924ef5ab338b19947b210c8
fridayshoes <one_amp@hotmail.com>
2026-01-07 10:44:22 +0000

    check is a word is that same when spelct  forwards or backwards

---
+++ str_palindrome.py
@@ -0,0 +1,13 @@
+# Check if a string is a palidrome (is spelt the same forwards and backwards)
+
+word = "madam"
+
+if word == word[::-1]:
+  print(word, "is a palindrome")
+else:
+  print(word, "is not a palindrome.")
+ 
+
+ # Output
+
+ # madam is a palindrome
