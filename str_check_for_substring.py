9ac2eb6f3550076b84563d102eace737950c8b41
fridayshoes <one_amp@hotmail.com>
2026-01-07 11:44:42 +0000

    find substring in a string

---
+++ str_check_for_substring.py
@@ -0,0 +1,14 @@
+# checl is 'cat' is in the string
+
+sentence = "The cat is on the mat"
+
+if "cat" in sentence:
+  print("Substring found")
+else:
+  print("Substring not found")
+
+
+
+# Output
+
+# Substring found
