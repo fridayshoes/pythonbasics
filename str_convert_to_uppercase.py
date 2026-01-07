44dfe13984b691c71c0d83ef2823e8358cdafd1d
fridayshoes <one_amp@hotmail.com>
2026-01-07 11:41:07 +0000

    convert string to uppercase

---
+++ str_convert_to_uppercase.py
@@ -0,0 +1,13 @@
+# Convert string to uppercase
+
+s = "hello"
+
+if s.isupper():
+  print("Already uppercase:", s)
+else:
+  print("Converted text:", s.upper())
+
+
+# Output
+
+# Converted text: HELLO
