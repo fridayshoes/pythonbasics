37ac57cc3e8cfa844a4cdbbc03dd961347e55629
fridayshoes <one_amp@hotmail.com>
2026-01-08 22:18:50 +0000

    List example for finding an item in a list

---
+++ list_find_item_in_list.py
@@ -0,0 +1,12 @@
+# Check if an item exists
+
+# Objective: Search for an item using if-else.
+# Outcome: Prints “Item found in list.”
+
+fruits = ["apple", "banana", "cherry"]
+if "banana" in fruits:
+    print("Item found in list.")
+else:
+    print("Item not in list.")
+
+# Item found in list.
