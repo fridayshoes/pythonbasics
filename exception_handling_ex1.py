b42d8b48ea1f147a37e6713fa53a8635248d1574
fridayshoes <one_amp@hotmail.com>
2026-01-11 15:57:52 +0000

    Except ValueError for entering age as non-integer, and incorrect age range

--- exception_handling_ex1.py
+++ exception_handling_ex1.py
@@ -6,8 +6,18 @@
 
 while True:
   try:
-    age_input = int(input('Please enter your age')) # int used as code must an integer
-    print(f"Your age is {age_input}") # output if someone enter '4'
+    age_input = int(input('Please enter your age: ')) # int used as code must an integer
+    if age_input <= 0:
+      raise ValueError("Age cannot be negative")
+    if age_input >= 123:
+      raise ValueError("Are you joking? Nobody has lived past 122 years!")
+    else:
+      print(f"Your age is {age_input}") # output if someone enter '4'
     break
-  except ValueError: # activates if someone enters 'four'
-    print("Invalid input. Enter numeric value for your age") # prints this error
+  except ValueError as e:
+        # This catches letters, symbols, or our custom negative age error
+        print(f"Invalid input: {e}")
+        print("Only numbers between 1 and 122 are acceptable")
+  
+  # except ValueError: # activates if someone enters 'four'
+  #   print("Invalid input. Enter numeric value for your age") # prints this error
